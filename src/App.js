f2036cff509a3db5bb9467282d6d22cc1dcff521
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-14 13:36:53 +0200

    weather name and location

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         .then(result => {
           setWeather(result);
           setQuery('');
+          console.log(result)
         })
     }
   }
@@ -45,22 +46,20 @@ function App() {
             onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">
-            New Yourk City, US
-          </div>
-          <div className="date">
-            {dateBuilder(new Date())}
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
           <div className="weather-box">
             <div className="temp">
-              15C
+              {Math.round(weather.main.temp)}°c
             </div>
-            <div className="weather">
-              Sunny
-            </div>
+            <div className="weather">{weather.weather[0].main}</div>
           </div>
         </div>
+        ) : ('')}
       </main>
     </div>
   );
